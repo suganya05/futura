da7cb54c3ed68b60b1d8e263a7de23da1f141092
kowshikeys <go2kowshiktouch@gmail.com>
2022-10-16 21:43:42 +0530

    footer

---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import Footer from "../Footer/Footer";
+import styles from "../styles/Home.module.css";
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <Footer />
+    </>
+  );
+};
+
+export default Home;
--- src/components/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as Header } from "./Header";
-export { default as Button } from "./Button";
-export { default as Hero } from "./Hero";
-export { default as Footer } from "./Footer";
